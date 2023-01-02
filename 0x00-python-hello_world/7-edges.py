5d2cf819bebc3620c3638950ef52cfa911c92fd3
Amanbel <amanuel11belachew@gmail.com>
2023-01-02 12:18:59 +0300

    py_print

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 w1 = word[:3]
-w2 = word[7:]
-w3 = word[1:8]
+w2 = word[-2:]
+w3 = word[1:-1]
 print(f"First 3 letters: {w1}")
 print(f"Last 2 letters: {w2}")
 print(f"Middle word: {w3}")
