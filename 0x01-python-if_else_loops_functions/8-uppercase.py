4d3c7fe64f063dc0bd564ac74284eaf1b461f31d
Amanbel <amanuel11belachew@gmail.com>
2023-01-03 15:01:48 +0300

    turn_upp_case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        char = ord(i)
+        if ord(i) >= 97 and ord(i) <= 122:
+            char = char - 32
+        print("{}".format(chr(char)),end="")
+    print()
