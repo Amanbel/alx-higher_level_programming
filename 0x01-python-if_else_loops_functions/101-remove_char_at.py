f30a1b1dec83c7997389c177f3d820b2431a254c
Amanbel <amanuel11belachew@gmail.com>
2023-01-03 15:48:24 +0300

    rm_char_py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str2 = str[0:n] + str[n + 1:len(str)]
-    return str2
+    if n > 0:
+        str2 = str[0:n] + str[n + 1:len(str)]
+        return str2
+    else:
+        return str
