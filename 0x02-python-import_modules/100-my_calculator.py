be1e5c3a50848016cf92721399771eed1da5452a
Amanbel <amanuel11belachew@gmail.com>
2023-01-05 10:23:13 +0300

    dyn_calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+    from calculator_1.py import add, sub, mul, div
+    if len(sys.argv) == 4:
+        val1 = int(sys.argv[1])
+        val2 = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print("{} + {} = {}".format(val1, val2, add(val1, val2)))
+        elif sys.argv[2] == '-':
+            print("{} - {} = {}".format(val1, val2, sub(val1, val2)))
+        elif sys.argv[2] == '*':
+            print("{} * {} = {}".format(val1, val2, mul(val1, val2)))
+        elif sys.argv[2] == '/':
+            print("{} + {} = {}".format(val1, val2, div(val1, val2)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
