34be52da73d06537c7582f4039f32911adde5838
Amanbel <amanuel11belachew@gmail.com>
2023-01-05 11:18:44 +0300

    dyn_calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
         val1 = int(sys.argv[1])
         val2 = int(sys.argv[3])
         if sys.argv[2] == '+':
-            print("{} + {} = {}".format(val1, val2, add(val1, val2)))
+            print("{:d} + {:d} = {:d}".format(val1, val2, add(val1, val2)))
         elif sys.argv[2] == '-':
-            print("{} - {} = {}".format(val1, val2, sub(val1, val2)))
+            print("{:d} - {:d} = {:d}".format(val1, val2, sub(val1, val2)))
         elif sys.argv[2] == '*':
-            print("{} * {} = {}".format(val1, val2, mul(val1, val2)))
+            print("{:d} * {:d} = {:d}".format(val1, val2, mul(val1, val2)))
         elif sys.argv[2] == '/':
-            print("{} + {} = {}".format(val1, val2, div(val1, val2)))
+            print("{:d} + {:d} = {:d}".format(val1, val2, div(val1, val2)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
