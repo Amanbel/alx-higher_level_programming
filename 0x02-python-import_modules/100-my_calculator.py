7dd8041ba8c98db9e424e80e79d501246d93df72
Amanbel <amanuel11belachew@gmail.com>
2023-01-05 13:15:59 +0300

    dynic_calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,23 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) == 4:
-        val1 = int(sys.argv[1])
-        val2 = int(sys.argv[3])
-        if sys.argv[2] == '+':
-            print("{:d} + {:d} = {:d}".format(val1, val2, add(val1, val2)))
-        elif sys.argv[2] == '-':
-            print("{:d} - {:d} = {:d}".format(val1, val2, sub(val1, val2)))
-        elif sys.argv[2] == '*':
-            print("{:d} * {:d} = {:d}".format(val1, val2, mul(val1, val2)))
-        elif sys.argv[2] == '/':
-            print("{:d} + {:d} = {:d}".format(val1, val2, div(val1, val2)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        exit(0)
-    else:
+    argv = sys.argv[1:]
+    argc = len(argv)
+    ops = ["+", "*", "/", "-"]
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    if str(sys.argv[2]) not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        val1 = int(sys.argv[1])
+        val2 = int(sys.argv[3])
+        if str(sys.argv[2]) == "+":
+            print("{:d} + {:d} = {:d}".format(val1, val2, add(val1, val2)))
+        elif str(sys.argv[2]) == "-":
+            print("{:d} - {:d} = {:d}".format(val1, val2, sub(val1, val2)))
+        elif str(sys.argv[2]) == "*":
+            print("{:d} * {:d} = {:d}".format(val1, val2, mul(val1, val2)))
+        elif str(sys.argv[2]) == "/":
+            print("{:d} / {:d} = {:f}".format(val1, val2, div(val1, val2)))
