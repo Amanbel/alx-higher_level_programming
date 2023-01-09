391cf62f1a8da840b32849867fc6ce6d066bc7d7
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 17:45:42 +0300

    data_py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) >= 2:
-        if len(tuple_a) == 0:
-            return tuple_b
-        t = (tuple_a[0] + tuple_b[0], tuple_b[1])
-        return t
-    if len(tuple_b) < 2 and len(tuple_a) >= 2:
-        if len(tuple_b) == 0:
-            return tuple_a
-        t = (tuple_a[0] + tuple_b[0], tuple_a[1])
-        return t
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
-        if len(tuple_a) == 0 and len(tuple_b) == 0:
-            return (0, 0)
-        if len(tuple_a) == 0 and len(tuple_b) == 1:
-            t = (tuple_b[0], 0)
-            return t
-        if len(tuple_a) == 1 and len(tuple_b) == 0:
-            t = (tuple_a[0], 0)
-            return t
-        t = (tuple_a[0], tuple_b[0])
-        return t
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        t = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
