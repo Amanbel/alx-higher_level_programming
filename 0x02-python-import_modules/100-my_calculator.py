60598f46bc797ac2da41d41ffdeb4a57dc69e1f1
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 23:59:08 +0300

    calc_dyn

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
     argv = sys.argv[1:]
     argc = len(argv)
