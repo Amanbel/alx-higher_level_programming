bf6d57cf3b9d0c9773eedac9c52cc75e5240fc1f
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 14:36:43 +0300

    data_py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= len(my_list) or idx < 0:
-        return None
 
     cp_list = my_list.copy()
+
+    if idx >= len(my_list) or idx < 0:
+        return cp_list
 
     return cp_list.insert(idx, element)
