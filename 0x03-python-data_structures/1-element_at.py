1ea384c602546ca648ddcf1bdc66041cfabbec29
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 13:29:42 +0300

    return_index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if (idx < 0):
         return None
-    if (idx >= len(my_list)):
+    if (idx > len(my_list)):
         return None
 
     ret_ind = my_list[idx, idx + 1]
