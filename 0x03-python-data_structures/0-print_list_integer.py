fefe5bf233aaaeded0609b41b54d69ddae85c5b2
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 13:17:14 +0300

    data_py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if (__name__ == "__main__"):
-    def print_list_integer(my_list=[]):
-        for i in my_list:
-            if (my_list == []):
-                print("{:d}".format(0))
-                break
-            print("{:d}".format(i))
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        if (my_list == []):
+            print("{:d}".format(0))
+            break
+        print("{:d}".format(i))
