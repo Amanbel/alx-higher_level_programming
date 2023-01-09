314cc15880d353da947bb544c135d17dd81b229a
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 14:50:50 +0300

    data_py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx > 0 and idx < len(my_list):
-        my_list[idx] = element
+        my_list[idx] = int(element)
 
     return my_list
