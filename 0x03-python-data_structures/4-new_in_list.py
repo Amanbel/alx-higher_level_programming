c266337b864c7959d3522b8c153df2c75f94ec15
Amanbel <amanuel11belachew@gmail.com>
2023-01-09 15:17:12 +0300

    data_py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cp_list = my_list[:]
-    if idx >= len(my_list) and idx < 0:
+    if idx < len(my_list) and idx >= 0:
         cp_list[idx] = element
 
     return cp_list
