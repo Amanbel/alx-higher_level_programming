3bb3790b32cfa3fdd0e23548f0bdb61e65229e14
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 16:38:14 +0300

    find_max

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    maxi = 0
+    for k in a_dictionary:
+        if maxi < a_dictionary[k]:
+            maxi = a_dictionary
+    for k in a_dictionary:
+        if maxi == a_dictionary[k]:
+            return k
