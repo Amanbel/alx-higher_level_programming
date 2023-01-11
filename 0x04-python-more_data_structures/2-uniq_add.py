fc595248851b00b31d0dc752425197f93c70009c
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 18:03:23 +0300

    uniq_add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     res = 0
-    map(lambda x: x if my_list.count(x) == 1 else my_list.pop(my_list.index(x)), my_list)
-    for i in my_list:
+    for i in set(my_list):
         res += i
     return res
