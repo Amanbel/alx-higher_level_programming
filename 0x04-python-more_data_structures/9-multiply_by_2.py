6b605180ad73dcfadd8f84d8adeeea515e666dcc
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 16:27:24 +0300

    chg_val

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary
+    new_dict = {}
 
-    for k, v in new_dict.items():
-        new_dict[k] = v * 2
+    for k in a_dictionary:
+        new_dict[k] = a_dictionary[k] * 2
     return new_dict
