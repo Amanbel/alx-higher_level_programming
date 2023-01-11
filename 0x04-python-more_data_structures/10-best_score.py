60505aa5fea54b7f775e56aba9245a1784d53390
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 16:40:59 +0300

    find_max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     maxi = 0
     for k in a_dictionary:
-        if maxi < a_dictionary[k]:
+        if maxi <= a_dictionary[k]:
             maxi = a_dictionary
     for k in a_dictionary:
         if maxi == a_dictionary[k]:
