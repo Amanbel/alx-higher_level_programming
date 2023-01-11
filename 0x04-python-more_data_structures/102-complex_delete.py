d31bc12fc490ecdadba3387ff175261cb4eb2752
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 17:35:38 +0300

    val_del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
-        if v == value:
-            del a_dictionary[k]
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
     return a_dictionary
