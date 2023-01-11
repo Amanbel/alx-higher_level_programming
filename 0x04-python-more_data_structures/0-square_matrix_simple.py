4eb53764050095ede637a3ca0687383c2c6bc5cc
Amanbel <amanuel11belachew@gmail.com>
2023-01-11 14:15:29 +0300

    sqr_mtrx

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n = 0
-    m = 0
-    new_list = []
-    for i in matrix:
-        for j in i:
-            new_list[n][m] = list(map(lambda x: x**2, matrix[n]))
-            m += 1
-        n += 1
+    new_list = [[0 for x in range(len(matrix[0]))] for y in range(len(matrix))]
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            new_list[i] = list(map(lambda x: x**2, matrix[i]))
     return new_list
