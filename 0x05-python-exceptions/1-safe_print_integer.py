293b6553f135599479caa82e1c5bdfc4b65c00c7
Amanbel <amanuel11belachew@gmail.com>
2023-01-23 12:07:29 +0300

    safe_integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value/2
-    except (TypeError, NameError):
+        print("{:d}".format(value))
+    except Exception as e:
         return False
     else:
-        print("{:d}".format(value))
         return True
