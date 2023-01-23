5ca4c1566b5280e1466affd97a0d3d544325032f
Amanbel <amanuel11belachew@gmail.com>
2023-01-23 11:46:14 +0300

    safe_print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for el in my_list[:x]:
             print("{}".format(el), end=""))
-        print('\n')
     except IndexError:
         pass
     finally:
-        return (i)
+        print()
+        return i
