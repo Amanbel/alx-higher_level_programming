86b732ab868a5eab69a1bf29bafc02dd7596f88c
Amanbel <amanuel11belachew@gmail.com>
2023-01-23 13:03:05 +0300

    int_count

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integer(my_list=[], x=0):
+    i = 0
+    for n in range(0, x):
+        try:
+            print("{:d}".format(my_list[n]))
+        except TypeError:
+            pass
+        else:
+            i += 1
+    print()
+    return i
