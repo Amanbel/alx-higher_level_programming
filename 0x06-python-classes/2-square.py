473b114d9ece7d3f601f76638083708a20de704e
Amanbel <amanuel11belachew@gmail.com>
2023-01-31 13:44:35 +0300

    sett

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,10 @@ class Square:
     """class which has a private size attribute"""
     def __init__(self, size=0):
         """initialization function"""
+        self.size = size
+    @size.setter
+    def size(self, size):
+        """property setter"""
         if isinstance(size, str):
             raise TypeError('size must be an integer')
         else if size < 0:
