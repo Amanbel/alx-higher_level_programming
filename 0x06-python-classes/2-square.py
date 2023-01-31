aa15c0f35f288d508d274e8661e8fa2885842b84
Amanbel <amanuel11belachew@gmail.com>
2023-01-31 13:31:44 +0300

    chk_private

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class accepts arguments"""
+
+
+class Square:
+    """class which has a private size attribute"""
+    def __init__(self, size=0):
+        """initialization function"""
+        if isinstance(size, str):
+            raise TypeError('size must be an integer')
+        else if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
