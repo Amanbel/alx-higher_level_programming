584229fca72bfbfa60073413eea9979a6d75b82c
Amanbel <amanuel11belachew@gmail.com>
2023-01-31 13:19:52 +0300

    private_att

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class accepts arguments"""
+
+
+class Square:
+    """class which has a private size attribute"""
+    def __init__(self, size):
+        """initialization function"""
+        self.__size = size
