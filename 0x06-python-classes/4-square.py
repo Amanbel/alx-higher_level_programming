edff4d5d3a7dda059645895586f0b42fb858a430
Amanbel <amanuel11belachew@gmail.com>
2023-01-31 14:17:30 +0300

    property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class accepts arguments"""
+
+
+class Square:
+    """class which has a private size attribute"""
+    def __init__(self, size=0):
+        """initialization function"""
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """function that calculates the area of the square"""
+        return self.__size ** 2
