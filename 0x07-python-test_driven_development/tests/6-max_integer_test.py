c8dd432a5132f9e97d93a332ce1b54c811ae2051
Amanbel <amanuel11belachew@gmail.com>
2023-02-04 17:56:13 +0300

    tst_fold

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""unittest module to test max_integer function"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TetsMaxInteger(unittest.TestCase):
+    """class with the unittests"""
+
+
+    def TestResults(self):
+        """function to test the result of the function"""
+        self.assertAlmostEqual(max_integer([1, 2, 4, 44]), 44)
+        self.assertAlmostEqual(max_integer([2, 88, 74, 5]), 88)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([]), None)
+
+    def TestValues(self):
+        """function to test the value input in the function"""
+        self.assertRaises(TypeError, max_integer, [1, 'e', 4, 5])
+        self.assertRaises(TypeError, max_integer, [1, "max", 3, 4])
