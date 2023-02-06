afa43ab50c33490ab647aae096fc6704d9aecae9
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 02:53:16 +0300

    class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""module with single class"""
+"""class names BaseGeometry"""
 
 
 class BaseGeometry:
-    """beta version of a class"""
     def area(self):
-        """area calculator"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """validates input integer"""
         if type(value) is not int:
-            raise TypeError(f'{name} must be an integer')
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+            raise ValueError("{} must be greater than 0".format(name))
