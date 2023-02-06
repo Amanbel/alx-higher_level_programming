4e00668d903e83e371850924fd5211c39d7bcf9c
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 01:16:06 +0300

    class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module with single class"""
+
+
+class BaseGeometry:
+    """beta version of a class"""
+    def area(self):
+        raise Exception('area() is not implemented')
