0c5e307e38908a9f5afdc63d4b064532878f8c78
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 01:05:12 +0300

    only_inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """checks if obj inherits from a_class"""
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
