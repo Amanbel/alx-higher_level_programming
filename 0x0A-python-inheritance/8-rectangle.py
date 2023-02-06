1f87b2936576f8b19eb451fb1396a473d6a1c960
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 02:35:28 +0300

    rec

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """module with a class of Rectangle"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class BaseGeometry:
+    """beta version of a class"""
+    def area(self):
+        """area calculator"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates input integer"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
 
 
 class Rectangle(BaseGeometry):
