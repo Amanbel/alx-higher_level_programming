45d21779d0113fac2e4182cf47943e121f1d2527
Amanbel <amanuel11belachew@gmail.com>
2023-02-06 23:26:28 +0300

    obj_list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module that holds a function for returning object lists"""
+
+
 def lookup(obj):
     """function that returns the object list of the argument"""
     return dir(obj)
