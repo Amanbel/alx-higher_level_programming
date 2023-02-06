8d1bba969a29aa5074f7a700c9d8388ab5f377fe
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 01:01:05 +0300

    only_inherits

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module with a single function"""
+
+
+def inherits_from(obj, a_class):
+    """checks if obj inherits from a_class"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
