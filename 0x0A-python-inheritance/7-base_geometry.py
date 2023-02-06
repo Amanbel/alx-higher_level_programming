2dbf1904fd7899b00780c0f0ad5a3ff390585da5
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 01:36:28 +0300

    class_prot_3

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module with single class"""
+
+
+class BaseGeometry:
+    """beta version of a class"""
+    def area(self):
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        """validates input integer"""
+        if type(value) != int:
+            raise TypeError('<name> must be an integer')
+        if value <= 0:
+            raise ValueError('<name> must be greater than 0')
