47263006d1d8b2d9b3262d12ec02263b232b8e70
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 00:55:17 +0300

    inherit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a module for a function"""
+
+
+
+def is_kind_of_class(obj, a_class):
+    """function checks if its inherited or not"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
