6cc56f0d76613cb409b12f8f7210874fa4d744bc
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 15:10:39 +0300

    attr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,10 +4,8 @@
 
 def add_attribute(inst, name_attr, value_attr):
     """function that sets a new attribut to an object instance"""
-    if type(inst) == str or type(inst) == int:
-        raise TypeError("can't add new attribute")
-    
-    if type(inst) == float or type(inst) == bool:
-        raise TypeError("can't add new attribute")
+    if hasattr(inst, "__dict__") or \
+            (hasattr(inst, "__slot__") and name_attr in inst.__slot__):
+        setattr(inst, name_attr, value_attr)
     else:
         setattr(inst, name_attr, value_attr)
