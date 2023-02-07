fb422127657f67ff725a3d56f1430d99a8b6b74a
Amanbel <amanuel11belachew@gmail.com>
2023-02-07 15:03:12 +0300

    attr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,9 +2,9 @@
 """function module"""
 
 
-def add_attribute(instance, name_attr, value_attr):
+def add_attribute(inst, name_attr, value_attr):
     """function that sets a new attribut to an object instance"""
-    if isinstance(instance, str) or isinstance(instance, int) or isinstance(instance, float) or isinstance(instance, bool):
+    if type(inst) == str or type(inst) == int or type(inst) == float or type(inst) == bool:
         raise TypeError("can't add new attribute")
     else:
-        setattr(instance, name_attr, value_attr)
+        setattr(inst, name_attr, value_attr)
