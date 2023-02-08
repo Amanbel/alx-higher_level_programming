07cd7a83fdd2881e91d9d5f4481d5f84efb7e6cb
Amanbel <amanuel11belachew@gmail.com>
2023-02-09 00:49:44 +0300

    self_dict

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module function"""
+
+
+class Student:
+    """class of student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(Self):
+        return self.__dict__
