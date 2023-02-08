52bc8ca59dd94a1d2b4759c74985848c57873cce
Amanbel <amanuel11belachew@gmail.com>
2023-02-08 05:11:38 +0300

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for a function"""
+
+
+def read_file(filename=""):
+    """reads from file and prints to stdout"""
+    with open(filename, "r", encoding="utf-8") as f
+    string = f.read()
+    print(string)
