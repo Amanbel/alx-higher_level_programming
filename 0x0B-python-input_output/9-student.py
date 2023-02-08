5365033831677f67657a3e4a729a6a27de2aa3a5
Amanbel <amanuel11belachew@gmail.com>
2023-02-09 00:50:51 +0300

    self_dict

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,5 +8,5 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    def to_json(Self):
+    def to_json(self):
         return self.__dict__
