5a3650baa7e96f67cbb7ed5a45e92f46271e5e08
Amanbel <amanuel11belachew@gmail.com>
2023-02-09 00:52:19 +0300

    self_dict

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,8 +5,11 @@
 class Student:
     """class of student"""
     def __init__(self, first_name, last_name, age):
+        """initializer function"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
+        """return dictionary of instanciated object attributes"""
         return self.__dict__
