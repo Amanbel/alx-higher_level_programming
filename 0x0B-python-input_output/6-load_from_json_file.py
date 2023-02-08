1968d0e483b39e5ddf1eced255504e2280927c02
Amanbel <amanuel11belachew@gmail.com>
2023-02-08 21:09:54 +0300

    json_file_to_obj

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """function opens json filename and deserializes it"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         obj = json.load(f)
         return obj
