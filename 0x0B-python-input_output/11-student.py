0f2045e2c8b84d623e033c2b288a6de2252cfd61
Amanbel <amanuel11belachew@gmail.com>
2023-02-09 02:07:44 +0300

    change

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module for a class"""
+
+
+class Student:
+    """class for student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a specific dictionary"""
+        if attrs is None:
+            return self.__dict__
+        dicts = {}
+        valu = self.__dict__
+        for key in attrs:
+            if key in valu:
+                dicts[key] = valu.get(key)
+            else:
+                pass
+        return dicts
+    
+    def reload_from_json(self, json):
+        """replaces attributes"""
+        self.__dict__.update(json)
