0b05ac82894ad8908785646e1d1aa771a9e9e2c3
Amanbel <amanuel11belachew@gmail.com>
2023-02-12 00:22:43 +0300

    rectan_four

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -38,14 +38,16 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
+        if self.height == 0 or self.width ==0:
+            return 0
         return (self.__height * 2) + (self.__width * 2)
 
     def __str__(self):
         if self.__heigth == 0 or self.__width == 0:
             return ''
         string = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
+        for i in range(self.height):
+            for j in range(self.width):
                 string += '#'
             string += '\n'
         return string
