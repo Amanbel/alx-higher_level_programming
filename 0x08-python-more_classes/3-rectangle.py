3c959844103bc6a4b669f361222905f32bc58409
Amanbel <amanuel11belachew@gmail.com>
2023-02-12 00:14:05 +0300

    recta_four

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""module of a class"""
+
+
+class Rectangle:
+    """class of Rectangle with area and perimeter metthod"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return (self.__height * 2) + (self.__width * 2)
+
+    def display_rec(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print('#', end="")
+            print()
+
+    def __str__(self):
+        return self.display_rec()
