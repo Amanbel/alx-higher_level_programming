b8561f1b7bff70e02f3d8d79c526b9419a145081
Amanbel <amanuel11belachew@gmail.com>
2023-02-12 22:08:53 +0300

    ans

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """module for a class"""
+
+
 class LockedClass:
     """a class that is allowed to have only one attribute"""
     def __setattr__(self, key, value):
         """a function which filters the attributes"""
         if key == 'first_name':
-            self.__dict__[key] = str(value)
+            self.__dict__[key] = value
         else:
-            raise AttributeError('\'LockedClass\' object has no attribute \'last_name\'')
+            raise AttributeError('\'LockedClass\' object has \
+                    no attribute \'last_name\'')
