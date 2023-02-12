c14154d9fe53601911e11b88a99f34094ff8e8d1
Amanbel <amanuel11belachew@gmail.com>
2023-02-12 22:07:08 +0300

    ans

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""module for a class"""
 class LockedClass:
+    """a class that is allowed to have only one attribute"""
     def __setattr__(self, key, value):
+        """a function which filters the attributes"""
         if key == 'first_name':
             self.__dict__[key] = str(value)
         else:
