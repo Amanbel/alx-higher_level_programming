fd6dfc3a02c421c7b180b6ffb757e2eb6077d9e2
Amanbel <amanuel11belachew@gmail.com>
2023-02-13 02:51:44 +0300

    circles

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""module of a class"""
+from .base import Base
+
+
+class Rectangle(Base):
+    """a subclass of Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        elif width <= 0:
+            raise ValueError('width must be > 0')
+        else:
+            self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        elif height <= 0:
+            raise ValueError('height must be > 0')
+        else:
+            self.__height = height
+
+    @x.setter
+    def x(self, x):
+        if type(x) != int:
+            raise TypeError('x must be an integer')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
+        else:
+            self.__x = x
+
+    @y.setter
+    def y(self, y):
+        if type(y) != int:
+            raise TypeError('y must be an integer')
+        elif y < 0:
+            raise ValueError('y must be >= 0')
+        else:
+            self.__y = y
