6cd637e32f5c2fa9166d756c8d45ab21b9aecf47
Amanbel <amanuel11belachew@gmail.com>
2023-02-15 01:49:54 +0300

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,12 +17,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """a static method that returns serialized json string"""
-        dict_lst = []
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        for dicts in list_dictionaries:
-            dict_lst.append(json.dumps(dicts))
-        return str(dict_lst)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
