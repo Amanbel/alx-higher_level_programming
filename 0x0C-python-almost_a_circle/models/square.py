b6a91032fc49100d9a8b1fc3efc2ce1162e49f0a
Amanbel <amanuel11belachew@gmail.com>
2023-02-14 14:53:47 +0300

    circles

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
             [print(" ", end="") for x in range(self.x)]
             [print('#', end="") for w in range(self.width)]
             print("")
-    
+
     def __str__(self):
         return '[Square] ({}) {}/{}\
  - {}'.format(self.id, self.x, self.y, self.width)
