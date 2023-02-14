8ea70015f90f9cef8b826eff3b1179a97363a619
Amanbel <amanuel11belachew@gmail.com>
2023-02-15 01:54:21 +0300

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,4 +30,4 @@ class Base:
             d = objs.to_dictionary()
             dict_list.append(d)
         with open(filename, 'w', encoding="utf-8") as f:
-            json.dump(dict_list, f)
+            json.dump(str(dict_list), f)
