d66818fef3400b4ea22fcb689bebd60e037f8b40
Amanbel <amanuel11belachew@gmail.com>
2023-02-14 14:50:32 +0300

    circles

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a module for a subclass"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """a subclass of rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def area(self):
+        """calculates area of square"""
+        return self.width * self.height
+
+    def display(self):
+        """function to display the square with # character"""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+        [print("") for y in range(self.y)]
+        for i in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print('#', end="") for w in range(self.width)]
+            print("")
+    
+    def __str__(self):
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.width)
