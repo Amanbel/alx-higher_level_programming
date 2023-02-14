87d1b2274c3f36cffe5ace50a928fd6e8dc1d1d4
Amanbel <amanuel11belachew@gmail.com>
2023-02-14 21:58:33 +0300

    circles

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or list_dictionaries is "":
+        if list_dictionaries is "":
             return "[]"
         return json.dumps(list_dictionaries)
