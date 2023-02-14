f52de2110080926f5de8bfe4c49964885ff8538e
Amanbel <amanuel11belachew@gmail.com>
2023-02-14 21:57:17 +0300

    circles

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,15 +6,16 @@ import json
 class Base:
     """a base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id == None:
+        if id is None:
             type(self).__nb_objects += 1
             self.id = self.__nb_objects
-        elif id != None:
+        else:
             self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if not list_dictionaries:
+        if list_dictionaries is None or list_dictionaries is "":
             return "[]"
         return json.dumps(list_dictionaries)
