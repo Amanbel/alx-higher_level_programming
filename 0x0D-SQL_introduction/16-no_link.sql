be3af9ecde9e75f0e11cb2c563c1019b5a386d7b
Amanbel <amanuel11belachew@gmail.com>
2023-03-14 23:48:15 +0300

    sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- sql query to display name that has value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
