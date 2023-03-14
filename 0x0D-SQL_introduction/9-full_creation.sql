3b92eb03297f097ba5306394a29088690a246412
Amanbel <amanuel11belachew@gmail.com>
2023-03-14 23:12:01 +0300

    sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- sql query which creates a table and injects it with data
+CREATE TABLE IF NOT EXISTS second_table (
+	id int,
+	name varchar(256),
+	score int
+	);
+INSERT INTO second_table VALUES (1, "John", 10)
+INSERT INTO second_table VALUES (2, "Alex", 3)
+INSERT INTO second_table VALUES (3, "Bob", 14)
+INSERT INTO second_table VALUES (4, "George", 8)
