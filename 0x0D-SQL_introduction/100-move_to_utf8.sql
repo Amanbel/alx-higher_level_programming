7df467d61ecf35a19b018a85836ba0424c85452d
Amanbel <amanuel11belachew@gmail.com>
2023-03-15 08:57:38 +0300

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,3 @@
 -- sql query to ALTER database
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-UPDATE first_table SET name COLLATE utf8_unicode_ci
