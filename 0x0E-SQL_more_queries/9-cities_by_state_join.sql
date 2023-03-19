2b1b873bc5675638afaf00fa019126aec740c4e0
Amanbel <amanuel11belachew@gmail.com>
2023-03-20 00:11:44 +0300

    sql2

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,2 @@
 -- sql query for JOIN statment
-SELECT cities.id, cities.name, states.name FROM
-cities JOIN states ON cities.id=states.id;
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities JOIN hbtn_0d_usa.states ON hbtn_0d_usa.cities.id=hbtn_0d_usa.states.id;
