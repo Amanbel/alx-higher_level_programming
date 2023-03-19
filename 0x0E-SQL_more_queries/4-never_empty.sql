a3a95eead432a68245fedda6960cca4054fe6787
Amanbel <amanuel11belachew@gmail.com>
2023-03-19 23:01:58 +0300

    sql2

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,2 @@
 -- sql query to create table with primary key
-CREATE TABLE IF NOT EXISTS id_not_null(id INT PRIMARY KEY, name VARCHAR(256));
-ALTER id SET DEFAULT 1;
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id));
