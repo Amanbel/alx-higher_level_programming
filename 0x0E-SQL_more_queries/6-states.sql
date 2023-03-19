84920d0c8ce121533f389367298b2387b0134bcd
Amanbel <amanuel11belachew@gmail.com>
2023-03-19 23:13:53 +0300

    sql2

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- sql query to create unique, auto_incrementing id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
