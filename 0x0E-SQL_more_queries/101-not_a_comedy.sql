f9f1f921bfb2255526e0c8ca1af87100c70d04fc
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 23:13:43 +0300

    sql2

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,5 +3,5 @@ SELECT title
 FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id=id
 JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_genres.name IS NOT "Comedy"
+WHERE tv_genres.name != "Comedy"
 ORDER BY title;
