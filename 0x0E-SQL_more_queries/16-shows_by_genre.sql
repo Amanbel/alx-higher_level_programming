968433db7587c095421d076266722e3c83bb6eeb
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 23:01:22 +0300

    sql2

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- sql query to FULL JOIN two tables
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
-RIGHT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
