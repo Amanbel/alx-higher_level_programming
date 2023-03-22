b34ca182a0a20b84ca0c5aed9f76dfa4ddfc96bb
Amanbel <amanuel11belachew@gmail.com>
2023-03-23 00:04:57 +0300

    sql2

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- sql query that list all shows with their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate)
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings
 ORDER BY SUM(tv_show_ratings.rate) DESC;
