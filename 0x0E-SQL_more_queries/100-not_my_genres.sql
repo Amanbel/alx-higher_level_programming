c55bcf77136075d0f0fda8bb2af1a2c3fcd5b8cb
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 23:52:25 +0300

    sql2

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,4 +7,4 @@ WHERE tv_genres.id NOT IN (
 	JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
 	JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 	WHERE tv_shows.title="Dexter")
-ORDER BY tv_genre.name;
+ORDER BY tv_genres.name;
