637b7b2821964f80a9e079fe6ebab63d031c1ef0
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 23:17:51 +0300

    sql2

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,11 @@
 -- sql query to show tv_shows title with out comedy
 SELECT title
 FROM tv_shows
+ORDER BY title
+MINUS
+SELECT title
+FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id=id
 JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_genres.name != "Comedy"
+WHERE tv_genres.name="Comedy"
 ORDER BY title;
