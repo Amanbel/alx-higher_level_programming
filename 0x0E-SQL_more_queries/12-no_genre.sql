b7d33e8e34c325e0bf90fefa5565f7e2c343ca37
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 20:25:27 +0300

    sql2

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- sql query for JOIN
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_show_genres.genre_id=NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
