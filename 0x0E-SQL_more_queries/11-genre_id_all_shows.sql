95d000282e55f0ddafda4050500f0f77cb39351c
Amanbel <amanuel11belachew@gmail.com>
2023-03-22 20:13:56 +0300

    sql2

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- sql query for JOIN
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
