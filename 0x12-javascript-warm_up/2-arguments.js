55b9dc7beaecee52d460cb6267c25b5e55c7b2ff
Amanbel <amanuel11belachew@gmail.com>
2023-05-08 23:12:31 +0300

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let args = process.argv;
 let out;
-if (args.length == 2) {
+if (process.argv.length == 2) {
 	out = "No argument";
-} else if (args.length == 3) {
+} else if (process.argv.length == 3) {
 	out = "Argument found";
-} else if (args.length > 3) {
+} else if (process.argv.length > 3) {
 	out = "Arguments found";
 }
 console.log(out);
