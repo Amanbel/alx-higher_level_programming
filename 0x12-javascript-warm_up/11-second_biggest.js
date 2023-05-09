1a4e2fed7213a142049878900d9e968a3ae6ae1f
Amanbel <amanuel11belachew@gmail.com>
2023-05-10 00:54:24 +0300

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 
 if (process.argv.length > 3) {
-  let ord = process.argv.slice(2);
-  let j = 0;
   let max = process.argv[2];
   for (let i = 0; i < process.argv.length; i++) {
     if (process.argv[i] > max) {
       max = process.argv[i];
-      ord[j] = process.argv[i];
-      j++;
     }
+  }
+  const ord = process.argv.slice(2);
+  const arr = ord.filter((item) => {
+    if (item < max) {
+      return Number(item);
+    }
+  });
+  const secMax = arr.reduce((a, b) => Math.max(a, b), -Infinity);
+  console.log(secMax);
 } else {
   console.log(0);
 }
