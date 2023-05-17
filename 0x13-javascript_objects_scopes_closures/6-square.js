4bcb461d1fc62ff0a7f687182ddaacb85b9cf498
Amanbel <amanuel11belachew@gmail.com>
2023-05-17 14:44:29 +0300

    js_obj

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const SquareOg = require('5-square.js');
+class Square extends SquareOg {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(parseInt(this.width)));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(parseInt(this.width)));
+      }
+    }
+  }
+}
+
+module.exports = Square;
