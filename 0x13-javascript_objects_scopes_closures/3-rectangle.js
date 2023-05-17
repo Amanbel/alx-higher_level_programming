1b7ec12ece9b88e0b29307ca56fd4e87afd17a2a
Amanbel <amanuel11belachew@gmail.com>
2023-05-17 14:20:51 +0300

    js_obj

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i <= this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(parseInt(this.width)));
     }
   }
