35e3d8826dbf1ece91404350153d5d566e261c17
Amanbel <amanuel11belachew@gmail.com>
2023-05-17 20:24:33 +0300

    js_obj

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const newDict = {};
+for (let i = 0; i < Object.keys(dict).length; i++) {
+  let holder = [];
+  for (let j = 0; j < Object.keys(dict).length; j++) {
+    if (Object.values(dict)[i] === Object.values(dict)[j]) {
+      holder.push(Object.keys(dict)[j]);
+    }
+  }
+  newDict[Object.values(dict)[i]] = holder;
+}
+
+console.log(newDict);
