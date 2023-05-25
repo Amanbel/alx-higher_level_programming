3f5df58a571fa28bdcdf1aafc27080c0634a6364
Amanbel <amanuel11belachew@gmail.com>
2023-05-25 17:28:38 +0300

    js_ws

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = process.argv[2];
+let j = 0;
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).films.length);
+    const obj = JSON.parse(body);
+    for (let i = 0; i < obj.results.length; i++) {
+      if (obj.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        j++;
+      }
+    }
+    console.log(j);
   }
 });
