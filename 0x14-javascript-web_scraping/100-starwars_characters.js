20d4a98971f76b8e684d5a532b2f094625f6e586
Amanbel <amanuel11belachew@gmail.com>
2023-07-25 17:24:52 +0300

    fix-list_characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,7 +5,7 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(url, (error, response, body) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
     const charsArr = JSON.parse(body).characters;
     for (let i = 0; i < charsArr.length; i++) {
@@ -15,7 +15,7 @@ request(url, (error, response, body) => {
         } else {
           console.log(JSON.parse(body).name);
         }
-      })
+      });
     }
   }
 });
